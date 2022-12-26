1cfc0aa5da250d7643d9d43f5c2ed13f139d5cad
Darren Chang <darren.ch12@gmail.com>
2022-12-26 01:11:50 -0500

    Add FDA.

--- 03_analysis_fda.R
+++ 03_analysis_fda.R
@@ -1,8 +1,9 @@
 # 03_analysis_fda.R
 source("setup.R")
-vroom("zhvi_us.csv")
+library(fda) # note this masks dplyr::select
+zhvi <- vroom("https://files.zillowstatic.com/research/public_csvs/zhvi/Metro_zhvi_uc_sfrcondo_tier_0.33_0.67_month.csv?t=1667088753")
 
-# plot MSAs on the same plot
+# visualize MSAs in function manner
 zhvi_msa <- zhvi %>% 
     pivot_longer(!c(RegionID, SizeRank, RegionName, RegionType, StateName)
         , names_to = 'date'
@@ -27,3 +28,36 @@ s <- zhvi_msa %>%
 s
 
 ggsave(str_c(fig_path, 'zhvi_fda_msa.png'), plot = s, bg = 'white')    
+
+# fit FDA model
+
+# create matrx inputs
+z_mat_input <- zhvi_msa %>% 
+    dplyr::select(-c(RegionName, RegionType, StateName, zhvi)) %>% 
+    drop_na() %>% 
+    filter(date >= '2002-01-01') %>% # find method for imputing growth rates
+    pivot_wider(names_from = date, values_from = yoy) %>% 
+    filter(if_all(everything(), ~!is.na(.))) 
+    
+# create matrix
+z_mat <- z_mat_input %>% 
+    dplyr::select(-SizeRank) %>% 
+    column_to_rownames("RegionID") %>% 
+    as.matrix()
+
+# create size factor
+size <- z_mat_input %>% 
+    dplyr::select(SizeRank) %>% 
+    .$SizeRank
+
+# fit model using FDA pkg
+z_mat_fxn <- fd(t(z_mat))
+mod <- fRegress(z_mat_fxn ~ size)
+
+# calculated yhat
+fitted <- t(as.matrix(mod$yhatfdobj$coefs))
+colnames(fitted) <- colnames(z_mat_input)[3:length(colnames(z_mat_input))]
+rownames(fitted) <- c()
+
+# calculate residuals
+resid <- z_mat - fitted
